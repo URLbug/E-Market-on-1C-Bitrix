a48f974421313d7b1606aafb96010005065abf71
Rui Carter <gemslove2@gmail.com>
2024-04-15 17:25:56 +0200

    Add new text

---
+++ include/html/AD_banner.php
@@ -0,0 +1,26 @@
+<section class="special-price">
+    <div class="price-intro">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="pull-left">
+                        <blockquote>
+                            <?php
+                            $APPLICATION->IncludeFile(
+                                    INCLUDE_PATH . '/AD_text.php',
+                                [],
+                                [
+                                        'MODE' => 'html',
+                                ],
+                            )
+                            ?>
+                        </blockquote>
+                    </div>
+                    <div class="pull-right"><a href="/shop" class="btn btn-default btn-lg"> Действуй!</a>
+                    </div>
+                </div>
+            </div> <!-- section title -->
+
+        </div>
+    </div>
+</section>
---
+++ local/components/cigars/cigars.newproduct/component.php
@@ -0,0 +1,64 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+
+/**
+ * @var array<string, mixed> $arParams
+ * @var CBitrixComponent $this
+ */
+
+CModule::IncludeModule('iblock');
+
+$arResult = [];
+
+$iblocks = CIBlockElement::GetList(
+    [],
+    [
+        'IBLOCK_ID' => $arParams['IBLOCK_TYPE_ID'],
+    ]
+);
+
+$obParser = new CTextParser();
+
+$iblocks->SetUrlTemplates($arParams["DETAIL_URL"], '', ($arParams["IBLOCK_URL"] ?? ''));
+
+$maxItem = 0;
+
+while($iblock = $iblocks->fetch())
+{
+    $props = CIBlockElement::GetProperty(
+        $arParams['IBLOCK_TYPE_ID'],
+        $iblock['ID'],
+        []);
+
+    while($prop = $props->Fetch())
+    {
+        if(
+            $prop['ID'] == 11 ||
+            $prop['ID'] == 2 ||
+            $prop['ID'] == 3
+        ) {
+            $iblock['PROPERTY'][$prop['CODE']][] = $prop;
+        }
+    }
+
+    if($iblock['PROPERTY']['RATING'][0]['VALUE'] >= 4 && $maxItem <= 4)
+    {
+        $arButtons = CIBlock::GetPanelButtons(
+            $iblock["IBLOCK_ID"],
+            $iblock["ID"],
+            0,
+            array("SECTION_BUTTONS" => false, "SESSID" => false));
+
+        $iblock["EDIT_LINK"] = $arButtons["edit"]["edit_element"]["ACTION_URL"] ?? '';
+        $iblock["DELETE_LINK"] = $arButtons["edit"]["delete_element"]["ACTION_URL"] ?? '';
+
+        $iblock['PREVIEW_PICTURE'] = CFile::GetFileArray($iblock['PREVIEW_PICTURE']);
+
+        $arResult['ITEMS'][] = $iblock;
+
+        $maxItem += 1;
+    }
+}
+
+$this->includeComponentTemplate();
